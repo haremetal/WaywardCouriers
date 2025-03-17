d20aad32f7e080626df8bf7031263d1b2d75440a
haremetal <griffin.bailey@gmail.com>
2025-03-17 21:23:37 +0800

    added health bars

--- Assets/Scripts/Goods Health.cs
+++ Assets/Scripts/Goods Health.cs
@@ -4,17 +4,21 @@ public class GoodsHealth : MonoBehaviour, IDamageable
 {
     [Header("Health Settings")]
     [SerializeField] private int maxHealth = 100;
-
+    
     private int currentHealth;
+
+    public HealthBar healthBar;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     public void Damage(int amount)
     {
         currentHealth -= amount;
+        healthBar.SetHealth(currentHealth);
         Debug.Log($"Our goods have taken {amount} damage! Current health: {currentHealth}");
 
         if (currentHealth <= 0)
--- Assets/Scripts/Player Health.cs
+++ Assets/Scripts/Player Health.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
-    [SerializeField] private int maxHealth = 10;
-    private int currentHealth;
+    [SerializeField] public int maxHealth = 10;
+    [SerializeField] public int currentHealth;
+
+    public HealthBar healthBar;
 
     [SerializeField] private Animator playerAnim;
 
@@ -11,12 +13,22 @@ public class PlayerHealth : MonoBehaviour, IDamageable
     {
         playerAnim = GetComponent<Animator>();
         currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Damage(1);
+        }
     }
 
     public void Damage(int damageAmount)
     {
         playerAnim.SetTrigger("hurt");
         currentHealth -= damageAmount;
+        healthBar.SetHealth(currentHealth);
         Debug.Log($"Player took {damageAmount} damage! Current health: {currentHealth}");
 
         if (currentHealth <= 0)
